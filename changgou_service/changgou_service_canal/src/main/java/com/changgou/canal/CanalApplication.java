aa5571c439a61b6c170dc660697dcea41c83a3d3
lqs <527089599@qq.com>
2021-01-25 09:27:14 +0800

    第四天内容

--- changgou_service/changgou_service_canal/src/main/java/com/changgou/canal/CanalApplication.java
+++ changgou_service/changgou_service_canal/src/main/java/com/changgou/canal/CanalApplication.java
@@ -1,13 +1,11 @@
 package com.changgou.canal;
 
-
 import com.xpand.starter.canal.annotation.EnableCanalClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
