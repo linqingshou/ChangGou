33547ba11dc9b0c879c04c3c7c6001dc619ef34d
lqs <527089599@qq.com>
2021-01-25 11:09:56 +0800

    第四天内容

--- changgou_service/changgou-service-content/src/main/java/com/changgou/content/ContentApplication.java
+++ changgou_service/changgou-service-content/src/main/java/com/changgou/content/ContentApplication.java
@@ -4,7 +4,6 @@ package com.changgou.content;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
--- changgou_service/changgou_service_file/src/main/java/com/changgou/file/FileApplication.java
+++ changgou_service/changgou_service_file/src/main/java/com/changgou/file/FileApplication.java
@@ -3,9 +3,10 @@ package com.changgou.file;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableEurekaClient
 public class FileApplication {
 
@@ -13,3 +14,4 @@ public class FileApplication {
         SpringApplication.run(FileApplication.class);
     }
 }
+
